e21ec33922ba85494e1d3e8fd3efa43a17464b1a
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-19 03:48:54 -0500

    Update package.json file

--- app.js
+++ app.js
@@ -6,13 +6,35 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const catalogRouter = require('./routes/catalog');
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://joshuafoti64:KDFJuKu0pyh7HkXK@cluster0.9tpdtqe.mongodb.net/inventory_application?retryWrites=true&w=majority&appName=Cluster0";
+
+const dev_db_url = "mongodb+srv://joshuafoti64:KDFJuKu0pyh7HkXK@cluster0.9tpdtqe.mongodb.net/inventory_application?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -22,6 +44,8 @@ async function main() {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(compression()); // Compress all routes
 
 app.use(logger('dev'));
 app.use(express.json());
