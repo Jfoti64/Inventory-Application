2b5adaa64451cdc8da94b59bc856a8b6f38e3e31
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-18 20:49:27 -0500

    Add category_form page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -34,9 +34,9 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Category create form on GET.
-exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create GET");
-});
+exports.category_create_get = (req, res, next) => {
+  res.render('category_form', { title: 'Create Category', category: {}, errors: [] });
+};
 
 // Handle Category create on POST.
 exports.category_create_post = asyncHandler(async (req, res, next) => {
